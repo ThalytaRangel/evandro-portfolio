2e65ae8b0d962ecf034a3d0582e7cbec16c632b2
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-03-12 20:41:52 -0300

    chore: initial config for portfolio

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+
+
+export function App() {
+  
+
+  return (
+    <div className="bg-background">
+      <h1 className="text-3xl font-bold text-details font-display ">
+        Hello world!
+      </h1>
+       
+    </div>
+  )
+}
+
+
