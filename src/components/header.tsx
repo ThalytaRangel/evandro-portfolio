75ae1abb84c22d782910faf52a8afa3e6c0e0961
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-03-13 18:19:12 -0300

    feat: add page header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 export function Header() {  
 
   return ( 
-    <header className="flex align-middle justify-between items-center p-6 border-b border-foreground/10">
+    <header className="flex justify-between items-center p-6 border-b border-foreground/10">
         <h1 className="text-2xl font-bold"><span className="text-details pe-0.5">&lt;</span>Evandro Damaso <span className="text-details pe-0.5">&frasl;&gt;</span></h1>
         <nav className="space-x-6">
           <a href="#home" className="hover:text-details"><span className="text-secondary pe-0.5">#</span>home</a>
