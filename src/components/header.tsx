bdaff3e0c46dd1c8b49d676c80c4add14ab2fb1d
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-03-14 20:36:43 -0300

    style: adjust header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,22 +6,23 @@ export function Header() {
                 <span className="text-details pe-0.5">&frasl;&gt;</span>
             </h1>
             <nav className="space-x-6">
-                <a href="#home" className="hover:text-details">
+                <a href="/home" className="hover:text-details">
                     <span className="text-secondary pe-0.5">#</span>home
                 </a>
-                <a href="#about-me" className="hover:text-details">
+                <a href="/about-me" className="hover:text-details">
                     <span className="text-secondary pe-0.5">#</span>about-me
                 </a>
-                <a href="#projects" className="hover:text-details">
-                    <span className="text-secondary pe-0.5">#</span>projects
-                </a>
-                <a href="#skills" className="hover:text-details">
+
+                <a href="/skills" className="hover:text-details">
                     <span className="text-secondary pe-0.5">#</span>skills
                 </a>
-                <a href="#experience" className="hover:text-details">
+                <a href="/projects" className="hover:text-details">
+                    <span className="text-secondary pe-0.5">#</span>projects
+                </a>
+                <a href="/experience" className="hover:text-details">
                     <span className="text-secondary pe-0.5">#</span>experience
                 </a>
-                <a href="#contacts" className="hover:text-details">
+                <a href="/contacts" className="hover:text-details">
                     <span className="text-secondary pe-0.5">#</span>contacts
                 </a>
             </nav>
