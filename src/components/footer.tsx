51510769eaa4487a50329021fe08b47bc29c539a
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-03-14 20:36:02 -0300

    feat: create footer page

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+export function Footer() {
+    return (
+        <footer className="border-t py-8 text-center">
+            <p>© 2025. Feito por Thalyta Rangel</p>
+            <div className="mt-4 flex justify-center gap-4">
+                <a href="#" target="_blank">
+                    LinkedIn
+                </a>
+                <a href="#" target="_blank">
+                    Instagram
+                </a>
+                <a href="#" target="_blank">
+                    GitHub
+                </a>
+            </div>
+        </footer>
+    )
+}
