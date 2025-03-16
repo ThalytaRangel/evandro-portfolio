2a556769befa23f5cde5c13b352a77329ec6ff91
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-03-16 03:03:14 -0300

    feat: add projects data and  styles

--- src/components/contacts-section.tsx
+++ src/components/contacts-section.tsx
@@ -1,40 +1,77 @@
+import { FaDiscord, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { MdOutlineMailOutline } from 'react-icons/md'
+import EvandroAvatar from '../assets/evandro_avatar.png'
+
 export function Contacts() {
     return (
         <section
             id="contacts"
-            className="font-display mb-24 flex flex-col lg:px-28"
+            className="font-display flex w-full flex-col lg:px-28"
         >
-            <div className="mb-12 flex max-w-full items-center gap-2">
+            <div className="mb-12 flex w-full items-center gap-2">
                 <h2 className="text-light-blue text-3xl font-normal whitespace-nowrap">
                     <span className="text-secondary pe-0.5">#</span>Contato
                 </h2>
                 <div className="border-b-primary flex h-0 w-full items-end border align-bottom"></div>
             </div>
-            <div>
-                <p className="mb-4">
-                    I’m interested in freelance opportunities. Feel free to
-                    contact me!
-                </p>
-                <div className="flex justify-center gap-6">
-                    <a
-                        href="https://www.linkedin.com/"
-                        target="_blank"
-                        className="btn"
-                    >
-                        LinkedIn
-                    </a>
-                    <a
-                        href="https://discord.com/"
-                        target="_blank"
-                        className="btn"
-                    >
-                        Discord
-                    </a>
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+                <div className="flex flex-col gap-4 lg:col-span-2">
+                    <p className="mb-4">
+                        Estou sempre em busca de novos desafios e oportunidades
+                        para aplicar minhas habilidades em desenvolvimento web.
+                        Se tiver alguma proposta ou quiser trocar ideias,
+                        sinta-se à vontade para entrar em contato!
+                    </p>
+                    <div className="border-comments flex flex-col justify-center gap-6 self-start border p-8">
+                        <h3 className="text-2xl font-semibold">
+                            Me acompanhe por aqui!{' '}
+                        </h3>
+                        <div className="flex gap-4">
+                            <a
+                                href="https://www.linkedin.com/in/evandro-damaso/"
+                                target="_blank"
+                                className="border-details hover:border-foreground flex h-16 w-16 items-center justify-center rounded-full border-1"
+                            >
+                                <FaLinkedin className="hover:text-primary text-3xl" />
+                            </a>
+                            <a
+                                href="https://github.com/dam450"
+                                target="_blank"
+                                className="border-details hover:border-foreground flex h-16 w-16 items-center justify-center rounded-full border-1"
+                            >
+                                <FaGithub className="hover:text-primary text-3xl" />
+                            </a>
+                            <a
+                                href="https://discord.com/users/547738105331843077"
+                                target="_blank"
+                                className="border-details hover:border-foreground flex h-16 w-16 items-center justify-center rounded-full border-1"
+                            >
+                                <FaDiscord className="hover:text-primary text-3xl" />
+                            </a>
+                            <a
+                                href="https://www.instagram.com/dam450.dev/"
+                                target="_blank"
+                                className="border-details hover:border-foreground flex h-16 w-16 items-center justify-center rounded-full border-1"
+                            >
+                                <FaInstagram className="hover:text-primary text-3xl" />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <p className="mt-8">
-                    Message me here:{' '}
-                    <span className="font-mono">elias@elias.me</span>
-                </p>
+                <div className="relative flex flex-col items-center gap-4 lg:col-span-1">
+                    <img
+                        src={EvandroAvatar}
+                        alt="Avatar de Evandro"
+                        className="h-64 w-64 rounded-lg object-cover"
+                    />
+                    <p className="border-comments absolute -bottom-3 mt-8 flex flex-col gap-2 border p-4">
+                        Entre em contato comigo:{' '}
+                        <span className="flex items-center gap-2 font-mono text-lg">
+                            {' '}
+                            <MdOutlineMailOutline /> evandro.damaso@gmail.com
+                        </span>
+                    </p>
+                </div>
             </div>
         </section>
     )
