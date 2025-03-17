575a97795d2b5eac0eb31f5dde0a9948d9b78faf
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-03-17 17:22:05 -0300

    feat: add more skills and adjust styles

--- src/components/skills-section.tsx
+++ src/components/skills-section.tsx
@@ -17,39 +17,51 @@ export function Skills() {
                     src={SkillsDoodles}
                     alt="Doodles da sessão"
                 />
-                <div className="grid gap-8 md:grid-cols-2">
-                    <div className="border-comments flex h-fit flex-col gap-2 border-2">
-                        <h3 className="text-foreground border-comments border-b-2 px-2 py-1 text-xl font-normal">
-                            Linguagens de programação e Marcação
-                        </h3>
-                        <p className="text-light-blue px-2 py-1">
-                            JavaScript, TypeScript, HTML e CSS
-                        </p>
-                    </div>
-                    <div className="border-comments flex h-fit flex-col gap-2 border-2">
-                        <h3 className="text-foreground border-comments mb-4 border-b-2 px-2 py-1 text-xl font-normal">
-                            Bancos de Dados e ORM
-                        </h3>
-                        <p className="text-light-blue px-2 py-1">
-                            Nodejs e Prisma
-                        </p>
-                    </div>
-                    <div className="border-comments flex h-fit flex-col gap-2 border-2">
-                        <h3 className="text-foreground border-comments mb-4 border-b-2 px-2 py-1 text-xl font-normal">
-                            Ferramentas e Editores
-                        </h3>
-                        <p className="text-light-blue px-2 py-1">
-                            Docker, Git, GitHub, VSCode e Figma
-                        </p>
-                    </div>
-                    <div className="border-comments flex h-fit flex-col gap-2 border-2">
-                        <h3 className="text-foreground border-comments mb-4 border-b-2 px-2 py-1 text-xl font-normal">
-                            Frameworks e Bibliotecas
-                        </h3>
-                        <p className="text-light-blue px-2 py-1">
-                            Reactjs, Nextjs, Astro, Nodejs, Flask, Sass,
-                            Tailwindcss Expressjs e Jest
-                        </p>
+                <div className="mx-auto max-w-7xl">
+                    <div className="grid grid-cols-1 gap-8 xl:grid-cols-3">
+                        <div className="border-comments row-span-2 flex h-fit flex-col gap-2 border-2 xl:min-h-52">
+                            <h3 className="text-foreground border-comments inline-block border-b-2 px-2 py-1 text-xl font-normal">
+                                Linguagens de programação e Marcação
+                            </h3>
+                            <p className="text-light-blue px-2 py-1">
+                                JavaScript, TypeScript, HTML e CSS
+                            </p>
+                        </div>
+                        <div className="border-comments flex flex-col gap-2 border-2">
+                            <h3 className="text-foreground border-comments border-b-2 px-2 py-1 text-xl font-normal">
+                                Frameworks e Bibliotecas
+                            </h3>
+                            <p className="text-light-blue px-2 py-1">
+                                Reactjs, Nextjs, Astro, Nodejs, Flask, Sass,
+                                Tailwindcss Expressjs e Jest
+                            </p>
+                        </div>
+
+                        <div className="border-comments flex flex-col gap-2 border-2">
+                            <h3 className="text-foreground border-comments border-b-2 px-2 py-1 text-xl font-normal">
+                                Bancos de Dados e ORM
+                            </h3>
+                            <p className="text-light-blue px-2 py-1">
+                                Nodejs e Prisma
+                            </p>
+                        </div>
+                        <div className="border-comments flex flex-col gap-2 border-2">
+                            <h3 className="text-foreground border-comments border-b-2 px-2 py-1 text-xl font-normal">
+                                Ferramentas e Editores
+                            </h3>
+                            <p className="text-light-blue px-2 py-1">
+                                Docker, Git, GitHub, VSCode e Figma
+                            </p>
+                        </div>
+                        <div className="border-comments flex flex-col gap-2 border-2">
+                            <h3 className="text-foreground border-comments flex-wrap border-b-2 px-2 py-1 text-xl font-normal">
+                                Liderança e Gerenciamento
+                            </h3>
+                            <p className="text-light-blue px-2 py-1">
+                                Líder técnico e gerente de projetos e
+                                coordenador de sprint
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
